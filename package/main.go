c699657b22bbb608207aa25a6c96ab22b7ac6d04
Devin <devintw16@gmail.com>
2021-06-29 23:13:28 +1000

    Test handler function for contact form

--- package/main.go
+++ package/main.go
@@ -7,11 +7,15 @@ import (
 
 func contact(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Contact form has been received")
+        r.ParseForm()
+        fmt.Println("",r.Form["email"])
+        fmt.Println("",r.Form["phone"])
+        fmt.Println("",r.Form["message"])
 }
 
 func main() {
         http.Handle("/", http.FileServer(http.Dir("../dist/")))
+        http.HandleFunc("/contact", contact )
         http.ListenAndServe(":8000", nil)
-
-		http.HandleFunc("/contact", contact )
+	
 }
